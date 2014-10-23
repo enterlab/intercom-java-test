ed578bbf725e0cfb7435d8584f33cc06fa4e8775
Henrik Mohr <henrik@meewee.com>
2014-10-23 15:59:20 +0200

    adding company to user

--- src/main/java/IntercomJavaTest.java
+++ src/main/java/IntercomJavaTest.java
@@ -1,8 +1,10 @@
 import java.util.HashMap;
+import java.util.Map;
 
 import io.intercom.api.Company;
 import io.intercom.api.CustomAttribute;
 import io.intercom.api.Intercom;
+import io.intercom.api.User;
 
 
 public class IntercomJavaTest {
@@ -11,19 +13,30 @@ public class IntercomJavaTest {
 		Intercom.setAppID("drlbmz91");
 		Intercom.setApiKey("f93795bbd9c4f3e6ecdff556d4e5e8ba3679dd87");
 		
-		Company company = new Company();
-	    company.setName("Blue Sun");
-	    company.setCompanyID("1");
-	    company.setPlan(new Company.Plan("premium"));
-	    company.addCustomAttribute(CustomAttribute.newIntegerAttribute("foddstuff-items", 246));
-	    company.addCustomAttribute(CustomAttribute.newStringAttribute("bestseller", "fruity oaty bar"));
-	    Company.create(company);
+//		Company company = new Company();
+//	    company.setName("Blue Sun");
+//	    company.setCompanyID("1");
+//	    company.setPlan(new Company.Plan("premium"));
+//	    company.addCustomAttribute(CustomAttribute.newIntegerAttribute("foddstuff-items", 246));
+//	    company.addCustomAttribute(CustomAttribute.newStringAttribute("bestseller", "fruity oaty bar"));
+//	    Company.create(company);
 	    
 	    HashMap<String, String> map = new HashMap<String, String>();
 	    map.put("name", "Blue Sun");
 	    Company company2 = Company.find(map);
 	    
 	    System.out.println(company2);
+	    
+	    Map<String, String> params = new HashMap<String, String>();
+	    params.put("email", "henrik@meew.ee");
+	    User user = User.find(params);
+	    
+	    System.out.println(user);
+	    
+	    user.addCompany(company2);
+	    User.update(user);
+	    
+	    
 	}
 
 }
